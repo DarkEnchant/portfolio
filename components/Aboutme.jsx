83300553b1a495c6d39736af924b76bb58579827
DarkEnchant <wenshen2002@gmail.com>
2023-12-10 17:30:02 +0800

    update

--- components/Aboutme.jsx
+++ components/Aboutme.jsx
@@ -57,9 +57,8 @@ const AboutMe = () => {
                 </div>
                 <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
                     <h1 className="text-4xl font-bold text-white mb-4">About Me</h1>
-                    <p className="text-base text-justify lg:text-lg">I am currently pursuing a degree in Cyber Security at APU, where I have been honing my skills in various technologies. I am familiar in frontend development, with expertise in React, Next.js, HTML, CSS, and I'm skilled in creating visually appealing and user-friendly interfaces using Tailwind CSS.
-                        Additionally, I've ventured into the world of blockchain development, particularly with Solidity, allowing me to build smart contracts for decentralized applications.
-                        My passion for technology and problem-solving has led me to actively participate in 11 hackathons this year, where I primarily serve as a Front End Developer. However, I am not limited to frontend tasks and often take on backend responsibilities, demonstrating my versatility and commitment to delivering complete solutions. </p>
+                    <p className="text-base text-justify lg:text-lg">Hello! I'm currently pursuing a degree in Cybersecurity at Asia Pacific University (APU), where I'm honing my skills to become a proficient cybersecurity expert. My academic journey in this dynamic field has not only deepened my understanding 
+                    of digital security but also fueled my passion for protecting digital infrastructures and data. </p>
                     <div className="flex flex-row mt-8">
 {/* 1e */}
                         <TabButton 
